b9509ebb2bd4164d20734f3060de69842dbcfaa7
Vlad <magickhero@gmail.com>
2019-11-26 16:32:19 +0300

    first commit

---
+++ resources/views/registerUser.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="addUser1" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    NAME : <input name="name">
+    EMAIL : <input name="email">
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    PASSWORD : <input name="password">
+    <input type="submit" name="submit"></input>
+</form>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('database-test', function () {
+    if (DB::connection()->getDatabaseName()) {
+        echo 'Connected succesfuly: ' . DB::connection()->getDatabaseName();
+    }
+});
+Route::get('People', 'tbl_usersController@index');
+
+Route::get('addUser', 'tbl_usersController@addUser');
+
+Route::post('/addUser1', 'tbl_usersController@addUserPost');
+
+Route::get('Bob/Bot', function () {
+
+    echo 'не хейти боба';
+});
